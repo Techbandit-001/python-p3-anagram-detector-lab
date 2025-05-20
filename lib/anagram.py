ba49d9ca22e975e2bd3f132ddc75faeb69df074e
Techbandit001 <davidkiptum80@gmail.com>
2025-05-20 12:24:13 +0300

    Implement Anagram class with match method

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        base_sorted = sorted(self.word)
+        return [
+            candidate for candidate in word_list
+            if sorted(candidate.lower()) == base_sorted and candidate.lower() != self.word
+        ]
+
